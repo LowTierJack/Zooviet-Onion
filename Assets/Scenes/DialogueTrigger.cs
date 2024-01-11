1543bac62ddf84d942fd7580fb1350aa243a7d5c
LowTierJack <jack.walden@elev.ga.lbs.se>
2024-01-11 12:56:39 +0100

    dialog

--- Assets/Scenes/DialogueTrigger.cs
+++ Assets/Scenes/DialogueTrigger.cs
@@ -7,5 +7,11 @@ public class DialogueTrigger : MonoBehaviour
 
     public Dialogue dialogue;
 
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+
 
 }
