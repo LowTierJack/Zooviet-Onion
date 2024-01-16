c2a85f33d79cbd133954c32187253f2613a8a6ec
LowTierJack <jack.walden@elev.ga.lbs.se>
2024-01-16 12:48:53 +0100

    dialogue fix

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -26,6 +26,7 @@ public class Dialogue : MonoBehaviour
                 if (Input.GetKey(KeyCode.Y))
             {
                 print("Ýes");
+                
                 gameObject.SetActive(false);
             }
 
